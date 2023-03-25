00ec7fac1c3535d0f88c1d664f931c1b77c49237
Morjello <morjello@yandex.ru>
2023-03-25 21:20:03 +0300

    g

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,11 @@ router.post(
   "/signin",
   celebrate({
     body: Joi.object().keys({
+      name: Joi.string().min(2).max(30),
+      about: Joi.string().min(2).max(30),
+      avatar: Joi.string().pattern(
+        /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/
+      ),
       email: Joi.string().required().email(),
       password: Joi.string().required().min(8),
     }),
