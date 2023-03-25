b3e30b2ef9439ba44a8ed0f43a69457fa99cd6d0
Morjello <morjello@yandex.ru>
2023-03-25 22:57:15 +0300

    Joy

--- routes/index.js
+++ routes/index.js
@@ -11,13 +11,15 @@ const { HTTPSAVE } = require("../utils/constants");
 router.post(
   "/signin",
   celebrate({
-    body: Joi.object().keys({
-      name: Joi.string().min(2).max(30),
-      about: Joi.string().min(2).max(30),
-      avatar: Joi.string().min(2).pattern(HTTPSAVE),
-      email: Joi.string().required().email(),
-      password: Joi.string().required().min(8),
-    }),
+    body: Joi.object()
+      .keys({
+        name: Joi.string().min(2).max(30),
+        about: Joi.string().min(2).max(30),
+        avatar: Joi.string().min(2).pattern(HTTPSAVE),
+        email: Joi.string().required().email(),
+        password: Joi.string().required(),
+      })
+      .unknown(true),
   }),
   login
 );
@@ -26,13 +28,15 @@ router.post(
 router.post(
   "/signup",
   celebrate({
-    body: Joi.object().keys({
-      name: Joi.string().min(2).max(30),
-      about: Joi.string().min(2).max(30),
-      avatar: Joi.string().pattern(HTTPSAVE),
-      email: Joi.string().required().email(),
-      password: Joi.string().required().min(8),
-    }),
+    body: Joi.object()
+      .keys({
+        name: Joi.string().min(2).max(30),
+        about: Joi.string().min(2).max(30),
+        avatar: Joi.string().pattern(HTTPSAVE),
+        email: Joi.string().required().email(),
+        password: Joi.string().required(),
+      })
+      .unknown(true),
   }),
   createUser
 );
